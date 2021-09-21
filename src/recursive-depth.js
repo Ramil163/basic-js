d10af1cff7c5aa8c1c6be707064653a5968f3461
Ramil <mustafin.rf@yandex.ru>
2021-09-21 23:01:51 +0400

    feat: dine recursive depth

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,20 +1,25 @@
-import { NotImplementedError } from '../extensions/index.js';
+import { NotImplementedError } from "../extensions/index.js";
 
 /**
  * Implement class DepthCalculator with method calculateDepth
  * that calculates deoth of nested array
- * 
+ *
  * @example
- * 
+ *
  * const depthCalc = new DepthCalculator();
  * depthCalc.calculateDepth([1, 2, 3, 4, 5]) => 1
  * depthCalc.calculateDepth([1, 2, 3, [4, 5]]) => 2
  * depthCalc.calculateDepth([[[]]]) => 3
  *
  */
+const hasNestedArray = (arr) => arr.some((item) => Array.isArray(item));
 export default class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    let counter = 1;
+    if (hasNestedArray(arr)) {
+      const arr1 = arr.reduce((acc, cur) => acc.concat(cur), []);
+      return counter + this.calculateDepth(arr1);
+    }
+    return counter;
   }
 }
